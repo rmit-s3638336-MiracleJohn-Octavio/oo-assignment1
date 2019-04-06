0522f2a4baf1dbc1c0733741d3e4eb38104b1675
Mj Octavio <mj_octaviojr@yahoo.com>
2019-04-06 12:01:30 +1100

    before implementing view

--- ants_vs_beettles/src/controller/Main.java
+++ ants_vs_beettles/src/controller/Main.java
@@ -3,6 +3,7 @@ package controller;
 import model.game_engine.GameEngine;
 
 public class Main {
+
     private static GameEngine gameEngine = new GameEngine();
 
     public static void main(String[] args) {
